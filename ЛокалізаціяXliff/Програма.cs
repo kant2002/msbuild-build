a5e606e227a05f9eac0ca8f80e9a1db03fdfdb73
Andrii Kurdiumov <kant2002@gmail.com>
2023-12-29 11:24:20 +0600

    Add project files.

---
+++ ЛокалізаціяXliff/Програма.cs
@@ -0,0 +1,9 @@
+﻿using ЛокалізаціяXliff;
+
+Консоль.НадрукуватиРядок(ГлобальніРядки.ПривітСвіт);
+
+#region Встановити українську культуру для потока
+Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("uk");
+#endregion
+
+Консоль.НадрукуватиРядок(ГлобальніРядки.ПривітСвіт);
---
+++ Українська/Українська.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Система
+{
+    public static class Консоль
+    {
+        public static void НадрукуватиРядок(string значення)
+        {
+            Console.WriteLine(значення);
+        }
+
+        public static void НадрукуватиРядок()
+        {
+            Console.WriteLine();
+        }
+    }
+}
