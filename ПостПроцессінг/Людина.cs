18d768684e5b34f126fac5f3174c4ea7e7c04df9
Andrii Kurdiumov <kant2002@gmail.com>
2023-12-30 16:07:48 +0600

    Приклад як би писали по старому INPC

--- ПостПроцессінг/Людина.cs
+++ ПостПроцессінг/Людина.cs
@@ -11,3 +11,37 @@ public class Людина : INotifyPropertyChanged
     public string Прізвище { get; set; }
     public string ПовнеІмя => $"{Імя} {Прізвище}";
 }
+
+/*
+public class ЛюдинаЯкБиВиПисалиСами : INotifyPropertyChanged
+{
+	public string ПовнеІмя => $"{Імя} {Прізвище}";
+	public string Імя
+	{
+		get => імя;
+		set
+		{
+			імя = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Імя)));
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ПовнеІмя)));
+		}
+	}
+	public string Прізвище
+	{
+		get => прізвище;
+		set
+		{
+			прізвище = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Прізвище)));
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ПовнеІмя)));
+		}
+	}
+
+	private string імя;
+	private string прізвище;
+
+	/// <inheritdoc />
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+}
+*/
