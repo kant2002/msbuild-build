815327be31959b656b7bae32ebba8f1fd212332b
Andrii Kurdiumov <kant2002@gmail.com>
2024-01-03 12:45:45 +0600

    Свій адаптер тестів

---
+++ КеруванняТестами.АдаптерТестів/CompilerVerifier.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace КеруванняТестами.АдаптерТестів;
+
+internal class CompilerVerifier
+{
+    private readonly string _sourceFolder;
+    private readonly string _container;
+
+    public CompilerVerifier(string container)
+    {
+        _container = Path.GetDirectoryName(container)!;
+        _sourceFolder = FindRootFolder(container);
+    }
+
+    public string OutDir => Path.Combine(_sourceFolder, "КеруванняТестами", "bin");
+    public string ObjDir => Path.Combine(_sourceFolder, "КеруванняТестами", "obj");
+    public string TestCaseDir => Path.Combine(_sourceFolder, "КеруванняТестами");
+    public string TargetFramework => "Net";
+    public string Configuration => "Release";
+    public string SourceRoot => _sourceFolder;
+
+    private static string FindRootFolder(string file)
+    {
+        var directory = Path.GetDirectoryName(file);
+        if (directory is null) throw new InvalidOperationException("Cannot run from root location");
+        var cesiumSolution = Path.Combine(directory, "msbuild-build.sln");
+        while (!File.Exists(cesiumSolution))
+        {
+            directory = Path.GetDirectoryName(directory);
+            if (directory is null) throw new InvalidOperationException("Cannot run from root location");
+            cesiumSolution = Path.Combine(directory, "msbuild-build.sln");
+        }
+
+        return directory;
+    }
+
+    public void VerifySourceCode(string sourceCodeFile, IMessageLogger logger)
+    {
+        logger.SendMessage(TestMessageLevel.Informational, $"Магічно перевіряємо {sourceCodeFile} на будь які проблеми.");
+        
+        // Це сімуляція проблеми виконання тесту.
+        if (sourceCodeFile.EndsWith("помилка.tst"))
+        {
+            throw new InvalidOperationException($"Файл {sourceCodeFile} це суцілна помилка.");
+        }
+    }
+}
---
+++ КеруванняТестами.АдаптерТестів/FileTestDiscovery.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace КеруванняТестами.АдаптерТестів;
+
+[FileExtension(".tst")]
+[FileExtension(".exe")]
+[FileExtension(".dll")]
+[DefaultExecutorUri(ExecutorUri)]
+[ExtensionUri(ExecutorUri)]
+public class FileTestDiscovery : ITestDiscoverer, ITestExecutor
+{
+    const string ExecutorUri = "executor://TestFileIntegrationTestExecutor";
+    static TestProperty TextFileProperty;
+    static FileTestDiscovery()
+    {
+        TextFileProperty = TestProperty.Register("TestFileIntegrationTestExecutor.TestFile", "Шлях до ісходного кода на DSL", typeof(string), typeof(FileTestDiscovery));
+    }
+
+    public void DiscoverTests(IEnumerable<string> containers, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+    {
+        foreach (var testCase in FindTestCases(containers, logger))
+        {
+            discoverySink.SendTestCase(testCase);
+        }
+    }
+
+    CancellationTokenSource? cts;
+    public void RunTests(IEnumerable<TestCase>? tests, IRunContext? runContext, IFrameworkHandle? frameworkHandle)
+    {
+        if (tests is null || frameworkHandle is null)
+        {
+            return;
+        }
+
+        cts = new CancellationTokenSource();
+        try
+        {
+            foreach (var testCase in tests)
+            {
+                ExecuteTestCase(testCase, frameworkHandle);
+            }
+        }
+        finally
+        {
+            cts = null;
+        }
+    }
+
+    public void RunTests(IEnumerable<string>? containers, IRunContext? runContext, IFrameworkHandle? frameworkHandle)
+    {
+        if (containers is null || frameworkHandle is null)
+        {
+            return;
+        }
+
+        cts = new CancellationTokenSource();
+        try
+        {
+            foreach (var testCase in FindTestCases(containers, frameworkHandle))
+            {
+                ExecuteTestCase(testCase, frameworkHandle);
+            }
+        }
+        finally
+        {
+            cts = null;
+        }
+    }
+
+    public void Cancel()
+    {
+        cts?.Cancel();
+    }
+
+    private static void ExecuteTestCase(TestCase testCase, IFrameworkHandle frameworkHandle)
+    {
+        var sourceCodeFile = (string)testCase.GetPropertyValue(TextFileProperty)!;
+        frameworkHandle.RecordStart(testCase);
+        TestResult testResult = new TestResult(testCase);
+        try
+        {
+            if (!File.Exists(sourceCodeFile))
+            {
+                frameworkHandle.SendMessage(TestMessageLevel.Warning, $"{sourceCodeFile} not found.");
+                testResult.Outcome = TestOutcome.NotFound;
+                return;
+            }
+
+            var verifier = new CompilerVerifier(testCase.Source);
+            verifier.VerifySourceCode(sourceCodeFile, frameworkHandle);
+            testResult.Outcome = TestOutcome.Passed;
+        }
+        catch (Exception ex)
+        {
+            testResult.Outcome = TestOutcome.Failed;
+            testResult.ErrorMessage = ex.Message;
+            testResult.ErrorStackTrace = ex.StackTrace;
+        }
+        finally
+        {
+            frameworkHandle.RecordResult(testResult);
+            frameworkHandle.RecordEnd(testCase, testResult.Outcome);
+        }
+    }
+
+    private IEnumerable<TestCase> FindTestCases(IEnumerable<string> containers, IMessageLogger logger)
+    {
+        List<string> directories = new();
+        foreach (var container in containers)
+        {
+            if (Path.GetExtension(container) == ".exe" || Path.GetExtension(container) == ".dll")
+            {
+                var directory = Path.GetDirectoryName(container);
+                if (directory is null || directories.Contains(directory)) continue;
+
+                var files = Directory.GetFiles(directory, "*.tst", SearchOption.AllDirectories);
+                foreach (var file in files)
+                {
+                    if (file.EndsWith(".ignore.tst") || file.EndsWith(".ігнор.tst")) continue;
+                    Console.WriteLine(file);
+                    var relativeFile = Path.GetRelativePath(directory, file);
+                    string testName = relativeFile.Substring(0, relativeFile.Length - 4).Replace('\\', '.').Replace('/', '.');
+                    var testCase = new TestCase(testName, new Uri(ExecutorUri), container);
+                    testCase.SetPropertyValue(TextFileProperty, file);
+                    yield return testCase;
+                }
+
+                directories.Add(directory);
+            }
+        }
+    }
+}
